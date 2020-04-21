fdf679354c07cc620e5d69adc6dfae234d6f88cc
Hamza Basrai <hamza.basrai@gmail.com>
2020-04-20 23:25:26 -0400

    3.17 Added put endpoint to update existing entries

--- server/index.js
+++ server/index.js
@@ -64,6 +64,22 @@ app.post("/api/persons", (req, res, next) => {
     .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+
+  // Note - This is a regular JS object not a Mongoose Model
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find((person) => person.id === id);
