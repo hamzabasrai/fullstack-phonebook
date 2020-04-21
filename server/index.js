ae0037f586d51e9cb9064747dd1c3d58b6128fd1
Hamza Basrai <hamza.basrai@gmail.com>
2020-04-20 23:06:35 -0400

    3.15 Delete entries from database

--- server/index.js
+++ server/index.js
@@ -70,13 +70,11 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    persons = persons.filter((person) => person.id !== id);
-  }
-
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => console.log(error));
 });
 
 // All remaining requests return the React app, so it can handle routing.
